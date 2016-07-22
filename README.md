d46fe55c86e5e6477375d218e9082951f03dec65
jainshubhi <shubhijain@caltech.edu>
2016-07-21 23:16:48 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ilustra
---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, request, render_template, redirect, url_for
+
+################################### CONFIG #####################################
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+################################### ROUTES #####################################
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
